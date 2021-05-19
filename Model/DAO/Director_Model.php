73d53d62a887adf1bf4d17590442f9a1fe3d66ed
Andres Ariza <adac021298@gmail.com>
2021-05-19 15:09:20 -0500

    Crear Coordinadores (Director)

--- Model/DAO/Director_Model.php
+++ Model/DAO/Director_Model.php
@@ -1,88 +1,71 @@
 <?php
 
-include_once "../Config/db._config.php";
+include_once "../../Config/db._config.php";
 
-class CoordinadorModel{
+class DirectorModel
+{
 
     //Atributo ppara manejar la conexion con la base de datos
     private $conexion;
 
-    public function __construct() {
-        $this->conexion=Conexion::conectar();
+    public function __construct()
+    {
+        $this->conexion = Conexion::conectar();
     }
 
-    //Metodo para Insertar Coordinador
-    public function insertarDirector($nombre_director, $correo_director, $codigo_director, $celular_director){
-        $query = "INSERT INTO director VALUES(NULL, :nombre_director, :correo_director, :codigo_director, :celular_director)";
-        $stmt = $this->conexion->prepare($query);
-        $stmt->bindParam(":nombre_director", $nombre_director);
-        $stmt->bindParam(":correo_director", $correo_director);
-        $stmt->bindParam(":codigo_director", $codigo_director);
-        $stmt->bindParam(":celular_director", $celular_director);
-
-        if(!$stmt->execute()){
-            $stmt->closeCursor();
-            return 0;
-        }
-        else{
-            $stmt->closeCursor();
-            return 1;
-        }
-    }
-
-    //Metodo para actualizar Coordinador
-    public function actualizarDirector($id_director, $correo_director, $celular_director){
-        $query = "UPDATE director SET correo_director=:correo, celular_director=:celular WHERE id_director=:id";
+    //Metodo para actualizar Director
+    public function actualizarDirector($id_director, $correo_director, $nombre_director)
+    {
+        $query = "UPDATE director SET correo_director=:correo, nombre_director=:nombre WHERE id_director=:id";
         $stmt = $this->conexion->prepare($query);
         $stmt->bindParam(":id", $id_director);
         $stmt->bindParam(":correo", $correo_director);
-        $stmt->bindParam(":celular", $celular_director);
+        $stmt->bindParam(":nombre", $nombre_director);
 
-        if(!$stmt->execute()){
-            $stmt->closeCursor();
-            return 0;
-        }
-        else{
-            $stmt->closeCursor();
-            return 1;
-        }
-    }
-
-    // Metodo que muestra el listado de los coordinadores
-    public function listarDirectores()
-    {
-        $query = "SELECT id_director, nombre_director, correo_director, codigo_director, celular_director FROM director";
-        $lista_director = NULL;
-        
-        $stmt = $this->conexion->prepare($query);
         if (!$stmt->execute()) {
             $stmt->closeCursor();
             return 0;
         } else {
-            while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $lista_director[] = $result;
-            }
-            $stmt->closeCursor();
-            return $lista_director;
-        }
-    }
-
-    // Metodo para eliminar Coordinador
-    public function eliminarDirector($id_director){
-        $query = "DELETE FROM director WHERE id_director = :id";
-        $stmt = $this->conexion->prepare($query);
-        $stmt->bindParam(":id", $id_director);
-
-        if(!$stmt->execute()){
-            $stmt->closeCursor();
-            return 0;
-        }
-        else{
             $stmt->closeCursor();
             return 1;
         }
     }
 
- 
+    // Metodo que muestra buscar director por ID
+    public function buscarDirector($id_director)
+    {
+        $query = "SELECT id_director, nombre_director, correo_director FROM director WHERE id_director=:id";
+        $datos_director = NULL;
+        $stmt = $this->conexion->prepare($query);
+        $stmt->bindParam(":id", $id_director);
+        if (!$stmt->execute()) {
+            $stmt->closeCursor();
+            return 0;
+        } else {
+            if ($stmt->rowCount() > 0) {
+                $datos_director = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+            $stmt->closeCursor();
+            return $datos_director;
+        }
+    }
 
+    // Metodo que permite buscar un director por el correo
+    public function buscarDirectorPorCorreo($correo_director)
+    {
+        $query = "SELECT id_director, nombre_director, correo_director FROM director WHERE correo_director=:correo";
+        $datos_director = NULL;
+        $stmt = $this->conexion->prepare($query);
+        $stmt->bindParam(":correo", $correo_director);
+        if (!$stmt->execute()) {
+            $stmt->closeCursor();
+            return 0;
+        } else {
+            if ($stmt->rowCount() > 0) {
+                $datos_director = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+            $stmt->closeCursor();
+            return $datos_director;
+        }
+    }
 }
