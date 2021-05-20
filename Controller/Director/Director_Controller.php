2326be5f6bab8c291df813a381aeecdbed09dedf
Diego Navas <dieg9928.dn@gmail.com>
2021-05-20 09:30:52 -0500

    Editar Director (Director)

--- Controller/Director/Director_Controller.php
+++ Controller/Director/Director_Controller.php
@@ -1,8 +1,29 @@
 <?php
-require_once '../../Model/DAO/Coordinador_Model.php';
+require_once '../../Model/DAO/Director_Model.php';
 
 function buscarDirector($id_director)
 {
     $obj_director_model = new DirectorModel();
     return $obj_director_model->buscarDirector($id_director);
+}
+
+if(isset($_POST['accion'])){
+    if($_POST['accion'] =='editar_director'){
+        $id_director = $_POST['id_director'];
+        $nombre_director = $_POST['nombre_director'];
+        $correo_director = $_POST['correo_director'];
+        $obj_director_model = new DirectorModel();
+        $rta = $obj_director_model->actualizarDirector($id_director, $correo_director, $nombre_director);
+        if($rta == 0){
+            $response['title'] = "Error al actualizar coordinador";
+            $response['state'] = "error";
+            $response['location'] = "perfil.php";
+        } else {
+            $response['title'] = "Director actualizado correctamente";
+            $response['state'] = "success";
+            $response['location'] = "perfil.php";
+        }
+        echo json_encode($response);
+        
+    }
 }
